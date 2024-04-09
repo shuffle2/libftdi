41b88b97291b98c805a56413bf285f16c13411ef
Shawn Hoffman <godisgovernment@gmail.com>
2024-04-09 09:00:55 -0700

    msvc: fix warnings by using all enum values in case statements

--- src/ftdi.c
+++ src/ftdi.c
@@ -2918,9 +2918,11 @@ static unsigned char type2bit(unsigned char type, enum ftdi_chip_type chip)
             }
         }
         case TYPE_230X: /* FT230X is only UART */
+        case TYPE_AM:
+        case TYPE_BM:
+        case TYPE_4232H:
         default: return 0;
     }
-    return 0;
 }
 
 /**
@@ -4625,7 +4627,7 @@ int ftdi_write_eeprom_location(struct ftdi_context *ftdi, int eeprom_addr,
     switch (ftdi->type)
     {
         case TYPE_BM:
-        case  TYPE_2232C:
+        case TYPE_2232C:
             chip_type_location = 0x14;
             break;
         case TYPE_2232H:
@@ -4635,6 +4637,9 @@ int ftdi_write_eeprom_location(struct ftdi_context *ftdi, int eeprom_addr,
         case TYPE_232H:
             chip_type_location = 0x1e;
             break;
+        case TYPE_AM:
+        case TYPE_R:
+        case TYPE_230X:
         default:
             ftdi_error_return(-4, "Device can't access unprotected area");
     }
